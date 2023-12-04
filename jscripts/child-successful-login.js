430fcf66d3b70795097adfc747d14cca0d741c55
Mildred Amores <msamores000@gmail.com>
2023-12-04 08:56:01 +0800

    added event-listeners to child successful login

---
+++ jscripts/child-successful-login.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var btnNext = document.getElementById('btn-next');
+
+
+    btnNext.addEventListener('click', function () {
+        window.location.href = 'child-dashboard.html';
+    });
+});

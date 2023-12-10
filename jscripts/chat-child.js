f64a2380268dd16f39dbe8f68bfbe70406e2797f
Mildred Amores <msamores000@gmail.com>
2023-12-11 06:57:21 +0800

    added event listeners to call, v-call, and chat

---
+++ jscripts/chat-child.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var btnBack = document.getElementById('btn-back');
+
+    // Click event listener for btn-sms
+    btnBack.addEventListener('click', function () {
+        window.location.href = 'parent-home.html';
+    });
+
+
+});
+
+
+
---
+++ jscripts/video-calling.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var btnEnd = document.getElementById('btn-end');
+
+    // Click event listener for btn-sms
+    btnEnd.addEventListener('click', function () {
+        window.location.href = 'parent-home.html';
+    });
+
+
+});
+
+
+
